35f4583d1c7427995660e637fbc6a676d0d70334
Anton Sakovich <anton.sakovich@softteco.com>
2022-04-15 18:10:06 +0300

    Add `BaseFormValueConverter` to `MainWindow`

---
+++ BinaryCalculator.Wpf/BaseFormValueConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BinaryCalculator.Wpf
+{
+    public class BaseFormValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int number && targetType == typeof(string))
+            {
+
+                return System.Convert.ToString(number, 2);
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
