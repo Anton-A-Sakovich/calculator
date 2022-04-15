177051c0da3d6f0616f9151ccd97b6e23da01bf5
Anton Sakovich <anton.sakovich@softteco.com>
2022-04-15 17:33:09 +0300

    Add `BinaryCalculatorServiceCollectionExtensions`

---
+++ BinaryCalculator/BinaryCalculatorServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using BinaryCalculator;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class BinaryCalculatorServiceCollectionExtensions
+    {
+        public static IServiceCollection AddBinaryCalculator(this IServiceCollection services)
+        {
+            services.AddTransient<IBinaryCalculator, BinaryCalculator.BinaryCalculator>();
+            return services;
+        }
+    }
+}
